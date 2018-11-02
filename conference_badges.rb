9c503a2da40116ea50459b1f4d76581f722aecb9
Greg Morgan <gregm9999@gmail.com>
2018-11-02 19:47:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 # Write your code here.
 
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  # return "Hello, my name is #{name}."
+  name.each {|item| return "Hello, my name is #{name}."}
 end
 
 def batch_badge_creator(name)
