ad3ea5d2268c7e5e8a1e6ad6da5ed21fa45f2332
Greg Morgan <gregm9999@gmail.com>
2018-11-02 19:56:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,8 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(name)
-  name.each_with_index{|element, index| return "Hello, #{element}! You'll be assigned to room #{index+1}!"}
+  let myAr=[]
+  name.each_with_index{|element, index| myAr << return "Hello, #{element}! You'll be assigned to room #{index+1}!"}
 end
 
 def printer(name)
