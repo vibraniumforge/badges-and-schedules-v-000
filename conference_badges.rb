6c61e9e18378bf1d85e0e41ca44e3e5fae4c54b5
Greg Morgan <gregm9999@gmail.com>
2018-11-02 19:57:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(name)
   let myAr=[]
-  name.each_with_index{|element, index| myAr << return "Hello, #{element}! You'll be assigned to room #{index+1}!"}
+  name.each_with_index{|element, index| myAr <<  "Hello, #{element}! You'll be assigned to room #{index+1}!"}
 end
 
 def printer(name)
