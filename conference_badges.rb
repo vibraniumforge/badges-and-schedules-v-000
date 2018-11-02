9aaa06f0103b216c29b2ef0a81bd2f9474c8e235
Greg Morgan <gregm9999@gmail.com>
2018-11-02 18:50:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name.each{|name|  return badge_maker(name) }
+end
+
+def assign_rooms(name)
+  name.each_with_index{|element| return "Hello, #{element}! You'll be assigned to room #{index}!"}
+end
+
+def printer(name)
+  puts batch_badge_creator(name), assign_rooms(name)
+end
